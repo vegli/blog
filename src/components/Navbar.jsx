eff2c60e82c97a1a6053bb19ef3091a2ed71c259
Veljko Gligorijevic <gligorijevicveljko3@gmail.com>
2022-10-25 23:52:53 +0200

    Komit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="container">
+        <div className="logo">logo</div>
+        <div className="links">links</div>
+      </div>
+    </div>
   )
 }
 
