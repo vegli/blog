c671ceedad8982504534d08b2501fadedbb1607f
Veljko Gligorijevic <gligorijevicveljko3@gmail.com>
2022-11-12 21:04:01 +0100

    SQL Baza povezivanje, Fetchovanje podataka postova

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -69,5 +69,8 @@ export const login = (req,res) => {
 }
 
 export const logout = (req,res) => {
-    
+    res.clearCookie("access_token",{
+        sameSite:"none",
+        secure:true
+    }).status(200).json("Odjavljeni korisnik!")
 }
--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,39 @@
+import {db} from "../db.js"
+
+export const getPosts = (req,res) => {
+    const q = req.query.cat
+    ? "SELECT * FROM posts WHERE category = ?"
+    : "SELECT * FROM posts"
+
+    db.query(q, [req.query.cat], (err,data) => {
+        if(err) return res.send(err)
+
+        return res.status(200).json(data)
+    })
+}
+
+
+
+
+export const getPost = (req,res) => {
+    const q = "SELECT `username`,`title`,`desc`,p.img, u.img AS userImg,`category`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ?"
+
+    // params ovde je onaj id u URL vidi posts.js u rute
+    db.query(q,[req.params.id], (err,data) => {
+        if(err) return res.json(err)
+
+        return res.status(200).json(data[0])
+    })
+}
+
 export const addPost = (req,res) => {
+    res.json("From controller!")
+}
+
+export const deletePost = (req,res) => {
+    res.json("From controller!")
+}
+
+export const updatePost = (req,res) => {
     res.json("From controller!")
 }
