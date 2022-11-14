c935839f156704101284f29ce2d7b77ab24c6f9a
Veljko Gligorijevic <gligorijevicveljko3@gmail.com>
2022-11-14 13:39:00 +0100

    Update postova, upload slika

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,12 +1,13 @@
 import {db} from "../db.js"
+import jwt from "jsonwebtoken"
 
 export const getPosts = (req,res) => {
     const q = req.query.cat
-    ? "SELECT * FROM posts WHERE category = ?"
+    ? "SELECT * FROM posts WHERE cat = ?"
     : "SELECT * FROM posts"
 
     db.query(q, [req.query.cat], (err,data) => {
-        if(err) return res.send(err)
+        if(err) return res.status(500).send(err)
 
         return res.status(200).json(data)
     })
@@ -16,24 +17,81 @@ export const getPosts = (req,res) => {
 
 
 export const getPost = (req,res) => {
-    const q = "SELECT `username`,`title`,`desc`,p.img, u.img AS userImg,`category`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ?"
+    const q = "SELECT p.id, `username`,`title`,`desc`,p.img, u.img AS userImg,`cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ?"
 
     // params ovde je onaj id u URL vidi posts.js u rute
     db.query(q,[req.params.id], (err,data) => {
-        if(err) return res.json(err)
+        if(err) return res.status(500).json(err)
 
         return res.status(200).json(data[0])
     })
 }
 
 export const addPost = (req,res) => {
-    res.json("From controller!")
+    const token = req.cookies.access_token
+    if(!token) return res.status(401).json("Nemate pristup!")
+
+    jwt.verify(token,"jwtkey", (err,userInfo) => {
+        if(err) return res.status(403).json("Token nije validan!") 
+            const q = "INSERT INTO posts (`title`,`desc`,`img`,`cat`,`date`,`uid`)  VALUES(?)"
+            const values = [
+                req.body.title,
+                req.body.desc,
+                req.body.img,
+                req.body.cat,
+                req.body.date,
+                // token vraca user info koji sadrzi user id
+                userInfo.id
+            ]
+
+            db.query(q, [values], (err,data) =>{
+                if(err) return res.status(500).json(err);
+                return res.json("Kreiran post")
+            })
+    });
+
 }
 
 export const deletePost = (req,res) => {
-    res.json("From controller!")
+    const token = req.cookies.access_token
+    if(!token) return res.status(401).json("Nemate pristup!")
+
+    jwt.verify(token,"jwtkey", (err,userInfo) => {
+        if(err) return res.status(403).json("Token nije validan!")
+
+        const postId = req.params.id
+        const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?"
+
+        // Pogledaj auth.js u kontrolerima, vraca se id: data[0].id sto znaci da nam VRACA ID samo
+        db.query(q,[postId,userInfo.id], (err,data)=>{
+            if(err) return res.status(403).json("Ne mozete brisati ovaj post!")
+
+            return res.json("Obrisan post!")
+        })
+    })
 }
 
 export const updatePost = (req,res) => {
-    res.json("From controller!")
+    const token = req.cookies.access_token
+    if(!token) return res.status(401).json("Nemate pristup!")
+
+    jwt.verify(token,"jwtkey", (err,userInfo) => {
+        if(err) return res.status(403).json("Token nije validan!") 
+
+        const postId = req.params.id
+        // PROVERAVAMO identitet kreatora posta sa ovim uid = ?  !
+            const q = "UPDATE posts SET `title` = ?, `desc` = ?,`img` = ?,`cat` = ? WHERE `id` = ? AND `uid` = ?"
+            const values = [
+                req.body.title,
+                req.body.desc,
+                req.body.img,
+                req.body.cat,
+            ]
+
+            db.query(q, [...values, postId, userInfo.id], (err,data) =>{
+                if(err) return res.status(500).json(err);
+                return res.json("Azuriran post")
+            })
+    });
+
 }
--- api/index.js
+++ api/index.js
@@ -3,11 +3,31 @@ import postRoutes from "./routes/posts.js"
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
 import cookieParser from "cookie-parser"
+import multer from "multer"
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
+
+const storage = multer.diskStorage({
+    destination: function(req,file,cb) {
+        cb(null, '../client/blog/public/upload')
+    },
+    filename: function (req, file, cb) {
+        // Date.now() jer ista slika overwriteuje vec istu sliku, pa zato ovo da preventujemo radimo
+        cb(null, Date.now()+file.originalname)
+    }
+})
+
+const upload = multer({storage})
+app.post('/api/upload', upload.single('file'), function(req,res) {
+    const file = req.file;
+    res.status(200).json(file.filename)
+})
+
+
 app.use("/api/posts", postRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/auth", authRoutes)
